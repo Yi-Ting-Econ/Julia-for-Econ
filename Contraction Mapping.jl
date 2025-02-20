7ae427a8dbb54d333afd4e410734d0fee70ecb06
Yi-Ting Wang <wang3342@wisc.edu>
2025-02-21 05:28:48 +0800

    Update Contraction Mapping.jl

--- Contraction Mapping.jl
+++ Contraction Mapping.jl
@@ -1,13 +1,33 @@
-# Contraction Mapping #
+##########################################################
+# Finding the fixed point of a contraction #
+##########################################################
 
+# Define a mutable struct to store the result
 mutable struct Results
-    y::Float64
+    y::Float64 # Stores the current approximation of the fixed point
 end
 
+# Define a contraction mapping function
 function contraction(x::Float64)
     y = 0.5 * x + 2
     return y
 end
+
+# Function to find the fixed point of the contraction mapping
+#=
+    This function iteratively applies the contraction mapping to approximate
+    the fixed point. The process stops when the function value changes very little 
+    (below `tol`) or when the maximum number of iterations (`max_iter`) is reached.
+
+    Arguments:
+    - res: A mutable struct storing the current approximation of the fixed point.
+    - mapping: A function that represents the contraction mapping.
+    - tol: The tolerance level for convergence (default: 1e-10).
+    - max_iter: Maximum number of iterations allowed (default: 20,000).
+
+    Returns:
+    - The updated `Results` object with the estimated fixed point.
+=#
 
 function fixed_point_finder(res::Results, mapping; tol=1e-10, max_iter=20_000)
     iter = 0
@@ -39,13 +59,11 @@ function fixed_point_finder(res::Results, mapping; tol=1e-10, max_iter=20_000)
 end
 
 # Example usage
-res = Results(10.0)
+res = Results(10.0) # Initialize the result struct with an arbitrary starting value
 fixed_point_finder(res, contraction)
 println("Fixed point: ", res.y)
 
-# The initial guess should not matter
+# Try a different initial guess
 res = Results(18.0)
 fixed_point_finder(res, contraction)
 println("Fixed point: ", res.y)
-
-
